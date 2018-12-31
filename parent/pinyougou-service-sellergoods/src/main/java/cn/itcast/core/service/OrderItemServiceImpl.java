cfcfe916083882da2a2c75b57be792888ec2263e
Lusihang4612 <844486115@qq.com>
2018-12-31 10:11:24 +0800

    订单查询修改

--- parent/pinyougou-service-sellergoods/src/main/java/cn/itcast/core/service/OrderItemServiceImpl.java
+++ parent/pinyougou-service-sellergoods/src/main/java/cn/itcast/core/service/OrderItemServiceImpl.java
@@ -1,8 +1,11 @@
 package cn.itcast.core.service;
 
+import cn.itcast.core.dao.order.OrderDao;
 import cn.itcast.core.dao.order.OrderItemDao;
+import cn.itcast.core.pojo.order.Order;
 import cn.itcast.core.pojo.order.OrderItem;
 import cn.itcast.core.pojo.order.OrderItemQuery;
+import cn.itcast.core.pojo.order.OrderQuery;
 import com.alibaba.dubbo.config.annotation.Service;
 import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
@@ -15,13 +18,13 @@ import org.springframework.transaction.annotation.Transactional;
 public class OrderItemServiceImpl implements OrderItemService{
 
     @Autowired
-    private OrderItemDao orderItemDao;
+    private OrderDao orderDao;
     @Override
     public PageResult search(Integer page, Integer rows, String name) {
         PageHelper.startPage(page, rows);
-        OrderItemQuery orderItemQuery = new OrderItemQuery();
-        orderItemQuery.createCriteria().andSellerIdEqualTo(name);
-        Page<OrderItem> p = (Page<OrderItem>) orderItemDao.selectByExample(orderItemQuery);
+        OrderQuery orderQuery = new OrderQuery();
+        orderQuery.createCriteria().andSellerIdEqualTo(name);
+        Page<Order> p = (Page<Order>) orderDao.selectByExample(orderQuery);
         return new PageResult(p.getTotal(),p.getResult());
     }
 }
--- parent/pinyougou-web-shop/src/main/webapp/js/controller/orderItemController.js
+++ parent/pinyougou-web-shop/src/main/webapp/js/controller/orderItemController.js
@@ -2,7 +2,8 @@
 app.controller('orderItemController' ,function($scope,$controller,$location ,orderItemService){
 	
 	$controller('baseController',{$scope:$scope});//继承
-
+	//显示状态
+    $scope.status = ["未付款","已付款","未发货","已发货","交易成功","交易关闭","待评价"];
 	//分页
 	$scope.search=function(page,rows){
 		orderItemService.search(page,rows).success(
